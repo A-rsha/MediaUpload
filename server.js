5ffd4d9e76a7c74017e1f7316acf4e02ff28dad3
A-rsha <Fathimaarshak08@gmail.com>
2026-02-22 20:49:11 +0530

    render deploy ready

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-// MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/mediavault")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-// Model
+
 const MediaSchema = new mongoose.Schema({
   name: String,
   type: String,
@@ -20,7 +20,6 @@ const MediaSchema = new mongoose.Schema({
 
 const Media = mongoose.model("Media", MediaSchema);
 
-// Upload Route (No Multer Needed)
 app.post("/upload", async (req, res) => {
   try {
     const { name, type, data } = req.body;
@@ -40,12 +39,14 @@ app.post("/upload", async (req, res) => {
   }
 });
 
-// Get Media
+
 app.get("/media", async (req, res) => {
   const data = await Media.find();
   res.json(data);
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
